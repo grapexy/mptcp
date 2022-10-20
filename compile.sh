3ea1008df653e908db8c9e1dee44a7c9c5e69a21
grapexy <100813784+grapexy@users.noreply.github.com>
2022-10-20 09:42:08 +0400

    Apply patches

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+# Apply patches
+patch -p1 < kernel-patches/5.4/bbr2.patch
+
 MPTCP_VERSION=$(git symbolic-ref --short HEAD | grep -E -o "[0-9\.]+")
 make clean
 make x86_64_mptcp_defconfig
